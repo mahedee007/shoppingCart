56f9b5f9f02dc679facb968533ae317e68edc3af
Mahedee Hasan <mahedee007@gmail.com>
2023-08-02 01:35:10 +0600

    updated another funtion

--- js/case.js
+++ js/case.js
@@ -1,3 +1,40 @@
+function inputCaseTotal (isIncrease){
+    const inputNumberField = document.getElementById('case-number-field');
+  const inputNumberFieldString = inputNumberField.value;
+  const inputNumber = parseInt(inputNumberFieldString);
+  let updateInputTotal;
+  if(isIncrease){
+    updateInputTotal = inputNumber + 1;
+  }
+  else{
+    updateInputTotal = inputNumber - 1;
+  }
+  
+   inputNumberField.value = updateInputTotal;  
+   
+   return updateInputTotal;
+
+}
+function updateCasePrice(updateInputTotal){
+    const caseTotal = document.getElementById('case-total');
+    const caseTotalString = caseTotal.innerText;
+    const casePriceTotal = updateInputTotal * 59;
+    caseTotal.innerText = casePriceTotal;
+}
 document.getElementById('btn-case-plus').addEventListener('click', function(){
-    
+    const updateInputTotal = inputCaseTotal(true);
+//     const caseTotal = document.getElementById('case-total');
+// const caseTotalString = caseTotal.innerText;
+// const casePriceTotal = updateInputTotal * 59;
+// caseTotal.innerText = casePriceTotal;
+updateCasePrice(updateInputTotal);
+
+})
+document.getElementById('btn-case-minus').addEventListener('click', function(){
+    const updateInputTotal = inputCaseTotal(false);
+    // const caseTotal = document.getElementById('case-total');
+    // const caseTotalString = caseTotal.innerText;
+    // const casePriceTotal = updateInputTotal * 59;
+    // caseTotal.innerText = casePriceTotal;
+    updateCasePrice(updateInputTotal)
 })
--- js/phone.js
+++ js/phone.js
@@ -1,13 +1,44 @@
+function inputPhoneTotal (isIncrease){
+    const inputNumberField = document.getElementById('phone-number-field');
+  const inputNumberFieldString = inputNumberField.value;
+  const inputNumber = parseInt(inputNumberFieldString);
+  let updateInputTotal;
+  if(isIncrease){
+    updateInputTotal = inputNumber + 1;
+  }
+  else{
+    updateInputTotal = inputNumber - 1;
+  }
+  
+   inputNumberField.value = updateInputTotal;  
+   
+   return updateInputTotal;
 
+}
+function updatePhonePrice(updateInputTotal){
+    const phoneTotal = document.getElementById('phone-total');
+    const phoneTotalString = phoneTotal.innerText;
+    const phonePriceTotal = updateInputTotal * 1219;
+    phoneTotal.innerText = phonePriceTotal;
+    
+}
 document.getElementById('btn-phone-plus').addEventListener('click', function(){
 //   const inputNumberField = document.getElementById('phone-number-field');
 //   const inputNumberFieldString = inputNumberField.value;
 //   const inputNumber = parseInt(inputNumberFieldString);
 //   const inputTotal = inputNumber + 1;
 //    inputNumberField.value = inputTotal;  
-const updateInputTotal = inputTotal(true);
+const updateInputTotal = inputPhoneTotal(true);
+// const phoneTotal = document.getElementById('phone-total');
+// const phoneTotalString = phoneTotal.innerText;
+// const phonePriceTotal = updateInputTotal * 1219;
+// phoneTotal.innerText = phonePriceTotal;
+updatePhonePrice(updateInputTotal);
+
 })
 
 document.getElementById('btn-phone-minus').addEventListener('click', function(){
-    const updateInputTotal = inputTotal(false);
+    const updateInputTotal = inputPhoneTotal(false);
+    updatePhonePrice(updateInputTotal);
+   
 })
