163252bd5a23d47956cd66ad4e5ebdb0786dbe89
Mahedee Hasan <mahedee007@gmail.com>
2023-08-04 00:35:09 +0600

    final commit

--- js/common.js
+++ js/common.js
@@ -4,24 +4,33 @@ const cardPriceString = cardPrice.innerText;
 const cardPriceNumber = parseInt(cardPriceString);
 return cardPriceNumber;
 }
+
+function setElementbyID(Id, value){
+    const setElemnt = document.getElementById(Id);
+    setElemnt.innerText = value;
+}
+
 function calculateSubTotal(){
     const cardPhonePrice = getCardPrice('phone-total');
 const cardCasePrice = getCardPrice('case-total');
 // SubTotal
 const cardPriceNumber = cardPhonePrice + cardCasePrice;
-const subTotalPrice = document.getElementById('sub-total');
-const subPrice = cardPriceNumber;
-subTotalPrice.innerText = subPrice;
+// const subTotalPrice = document.getElementById('sub-total');
+
+// subTotalPrice.innerText = subPrice;
+setElementbyID('sub-total', cardPriceNumber);
 
 // Tax
-const taxTotal = document.getElementById('tax-amount');
-const taxAmountString = (subPrice * 0.01).toFixed(2);
+// const taxTotal = document.getElementById('tax-amount');
+const taxAmountString = (cardPriceNumber * 0.01).toFixed(2);
 const taxAmountNumber = parseFloat(taxAmountString);
-taxTotal.innerText = taxAmountNumber;
+// taxTotal.innerText = taxAmountNumber;
+setElementbyID('tax-amount', taxAmountNumber);
 
 // grandTotal
-const grandTotal = document.getElementById('final-total');
-const grandTotalAmount = subPrice + taxAmountNumber;
-grandTotal.innerText = grandTotalAmount;
+// const grandTotal = document.getElementById('final-total');
+const grandTotalAmount = cardPriceNumber + taxAmountNumber;
+// grandTotal.innerText = grandTotalAmount;
+setElementbyID('final-total', grandTotalAmount);
 
 }
